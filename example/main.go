581ce235c95fbcb1774509bbda15c54d4262b525
ysugimoto <sugimoto@wnotes.net>
2020-09-03 20:28:48 +0900

    tidy up example

--- example/main.go
+++ example/main.go
@@ -26,10 +26,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to read credential file %v\n", err)
 	}
+
 	config, err := google.ConfigFromJSON(buf, meet.MeetScopes...)
 	if err != nil {
 		log.Fatalf("Failed to create oauth2 config %v\n", err)
 	}
+
 	token, err := getTokenFromFile(tokenFile)
 	if err != nil {
 		token, err = getTokenFromWeb(config, tokenFile)
@@ -57,6 +59,7 @@ func getTokenFromFile(file string) (*oauth2.Token, error) {
 		return nil, fmt.Errorf("Failed to open token file %v", err)
 	}
 	defer fp.Close()
+
 	token := &oauth2.Token{}
 	if err := json.NewDecoder(fp).Decode(token); err != nil {
 		return nil, fmt.Errorf("Failed to decode saved json file %v", err)
@@ -75,15 +78,17 @@ func getTokenFromWeb(config *oauth2.Config, file string) (*oauth2.Token, error) 
 
 	token, err := config.Exchange(context.TODO(), authCode)
 	if err != nil {
-		log.Fatalf("Unable to retrieve token from web %v", err)
+		return nil, fmt.Errorf("Unable to retrieve token from web %v", err)
 	}
-	fp, err := os.OpenFile(file, os.O_CREATE|os.O_WRONLY, 0644)
+
+	// save to local file
+	fp, err := os.OpenFile(file, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to open token file %v", err)
 	}
 	defer fp.Close()
 	if err := json.NewEncoder(fp).Encode(token); err != nil {
-		return nil, fmt.Errorf("Failed to encode got json to file %v", err)
+		return nil, fmt.Errorf("Failed to encode token to json %v", err)
 	}
 	return token, nil
 }
